45625d64de78f4e1f09c4fc94adb4e3866ddebaf
Raj Gupta <119653833+rajguptaaa@users.noreply.github.com>
2025-10-22 12:03:32 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(cors({
   // origin: process.env.FRONTEND_URI || 'http://localhost:5173',
-  origin:'https://cipherstudio-frontend.onrender.com/';
+  origin:'https://cipherstudio-frontend.onrender.com/',
   credentials: true
 }));
 app.use(express.json());
